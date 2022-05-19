935a2acfc07179ffd6903c041e6257b68ba604b7
Jacob Czyz <jacobczyz03@gmail.com>
2022-05-18 21:54:00 -0500

    Day1 VSCODE

---
+++ Day 1/add.py
@@ -0,0 +1,6 @@
+print("Enter the first number: ", end='')
+val1 = int(input())
+print("Enter the second number: ", end='')
+val2 = int(input())
+added = val1 + val2
+print("The sum is " + str(added) + "!")
